2ac83613a352723a37d679caa154a025a8232963
elvinHeo <shintwl@icloud.com>
2023-04-15 17:56:20 +0900

    Revert "designed home view"
    This reverts commit b17edaa42d09e4a3fec59f1df318ea004ad57373.

---
+++ Wordy Wordy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wordy Wordy
+//
+//  Created by ShinIl Heo on 2023/04/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
